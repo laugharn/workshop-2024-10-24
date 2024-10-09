7bbfecf933292827092d7c16fbc214be6acc00d8
Dan Laugharn <laugharn@gmail.com>
2024-10-09 07:36:29 -0500

    🐛 Fixes error

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,6 +4,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     await res.revalidate(req.query.path as string)
     return res.json({ revalidated: true })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
     return res.status(500).send('Error revalidating')
   }
